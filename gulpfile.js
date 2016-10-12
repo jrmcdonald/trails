34f20dd70f4b32c3bb7c59117b8f6c00cb05f6e8
Jamie McDonald <jamie@qwyck.net>
2016-10-12 14:08:12 +0100

    Relocate css vendor directory.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var replace = require('gulp-replace');
  
 gulp.task('css', function () {
   gulp.src(['node_modules/leaflet*/dist/images/*.{png,svg}'])
-    .pipe(gulp.dest('client/vendor/'));
+    .pipe(gulp.dest('client/css/vendor/'));
   gulp.src(['node_modules/leaflet*/dist/leaflet*.css'])
     .pipe(concatCss("vendor.css"))
-    .pipe(replace('url("', 'url("../vendor/'))
+    .pipe(replace('url("', 'url("vendor/'))
     .pipe(gulp.dest('./client/css'));
 });
