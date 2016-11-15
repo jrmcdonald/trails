609b1d6ab8527b8d81c54d210396cd0883c540de
Jamie McDonald <jamie@qwyck.net>
2016-11-15 13:42:06 +0000

    Promisify startup.

--- server/server.js
+++ server/server.js
@@ -5,31 +5,22 @@ const setup = require('./setup');
 
 const app = module.exports = loopback();
 
-setup.cloudant('maps', (cloudantErr) => {
-  if (cloudantErr) {
-    console.log(cloudantErr);
-    process.exit(1);
-  }
-
-  setup.elevations((elevationsErr) => {
-    if (elevationsErr) {
-      console.log(elevationsErr);
-      process.exit(1);
+app.start = function () {
+  // start the web server
+  return app.listen(() => {
+    app.emit('started');
+    const baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    const componentExplorer = app.get('loopback-component-explorer');
+    if (componentExplorer) {
+      console.log('Browse your REST API at %s%s', baseUrl, componentExplorer.mountPath);
     }
+  });
+};
 
-    app.start = function () {
-      // start the web server
-      return app.listen(() => {
-        app.emit('started');
-        const baseUrl = app.get('url').replace(/\/$/, '');
-        console.log('Web server listening at: %s', baseUrl);
-        const componentExplorer = app.get('loopback-component-explorer');
-        if (componentExplorer) {
-          console.log('Browse your REST API at %s%s', baseUrl, componentExplorer.mountPath);
-        }
-      });
-    };
-
+setup.cloudant('maps')
+  .then(setup.elevations())
+  .then(() => {
     // Bootstrap the application, configure models, datasources and middleware.
     // Sub-apps like REST API are mounted via boot scripts.
     boot(app, __dirname, (err) => {
@@ -38,5 +29,8 @@ setup.cloudant('maps', (cloudantErr) => {
         app.start();
       }
     });
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
   });
-});
--- server/setup.js
+++ server/setup.js
@@ -3,63 +3,67 @@ const Cloudant = require('cloudant');
 
 const datasources = require('./datasources.json');
 
-exports.cloudant = function cloudant(database, callback) {
-  let cloudantURL = cfenv.getAppEnv().getServiceURL('trails-cloudantNoSQLDB', {}); 
-
-  if (!cloudantURL) {
-    cloudantURL = process.env.CLOUDANT_URL;
+exports.cloudant = function cloudant(database) {
+  return new Promise((resolve, reject) => {
+    let cloudantURL = cfenv.getAppEnv().getServiceURL('trails-cloudantNoSQLDB', {}); 
 
     if (!cloudantURL) {
-      callback('Error: No Cloudant URL defined. Must be defined in VCAP_SERVICES or local environment variables.');
+      cloudantURL = process.env.CLOUDANT_URL;
+
+      if (!cloudantURL) {
+        reject('Error: No Cloudant URL defined. Must be defined in VCAP_SERVICES or local environment variables.');
+      }
     }
-  }
 
-  console.log('Setting up Cloudant datasource.');
+    console.log('Setting up Cloudant datasource.');
 
-  const cloudantDb = new Cloudant(cloudantURL);
+    const cloudantDb = new Cloudant(cloudantURL);
 
-  cloudantDb.db.create(database, () => {
-    datasources.cloudant = {
-      name: 'cloudant',
-      connector: 'cloudant',
-      database,
-      url: cloudantURL,
-    };
+    cloudantDb.db.create(database, () => {
+      datasources.cloudant = {
+        name: 'cloudant',
+        connector: 'cloudant',
+        database,
+        url: cloudantURL,
+      };
 
-    callback(null);
+      resolve();
+    });
   });
 };
 
-exports.elevations = function elevations(callback) {
-  const bingApiKey = process.env.BING_KEY;
+exports.elevations = function elevations() {
+  return new Promise((resolve, reject) => {
+    const bingApiKey = process.env.BING_KEY;
 
-  if (!bingApiKey) {
-    callback('Error: No Bing Maps API Key defined. Must be defined in VCAP_SERVICES or local environment variables.');
-  }
+    if (!bingApiKey) {
+      reject('Error: No Bing Maps API Key defined. Must be defined in VCAP_SERVICES or local environment variables.');
+    }
 
-  console.log('Setting up Bing Elevations API datasource.');
+    console.log('Setting up Bing Elevations API datasource.');
 
-  datasources.elevations = {
-    name: 'elevations',
-    connector: 'rest',
-    operations: [
-      {
-        template: {
-          method: 'GET',
-          url: 'http://dev.virtualearth.net/REST/v1/Elevation/List',
-          query: {
-            points: '{points}',
-            key: bingApiKey,
+    datasources.elevations = {
+      name: 'elevations',
+      connector: 'rest',
+      operations: [
+        {
+          template: {
+            method: 'GET',
+            url: 'http://dev.virtualearth.net/REST/v1/Elevation/List',
+            query: {
+              points: '{points}',
+              key: bingApiKey,
+            },
+          },
+          functions: {
+            list: [
+              'points',
+            ],
           },
         },
-        functions: {
-          list: [
-            'points',
-          ],
-        },
-      },
-    ],
-  };
+      ],
+    };
 
-  callback(null);
+    resolve();
+  });
 };
