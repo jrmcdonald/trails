43e87213891651afa92edc43c7f6245b9b7b9c5f
Jamie McDonald <jamie@qwyck.net>
2017-01-12 10:25:39 +0000

    Untrack main.js and generate it postinstall.

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,26 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 
 gulp.task('connect', () => {
-  connect.server({ root: 'client', port: 8080 });
+  connect.server({ root: 'public', port: 8080 });
 });
 
 gulp.task('css', () => {
   gulp.src(['node_modules/leaflet*/dist/images/*.{gif,png,svg}'])
-    .pipe(gulp.dest('client/assets/lib/leaflet/'));
+    .pipe(gulp.dest('public/assets/lib/leaflet/'));
   gulp.src(['node_modules/leaflet*/dist/*.css'])
     .pipe(concat('leaflet.css'))
-    .pipe(gulp.dest('./client/assets/lib/leaflet'));
+    .pipe(gulp.dest('./public/assets/lib/leaflet'));
 });
 
 gulp.task('browserify', () => {
-  return browserify('./client/app/app.js')
+  return browserify('./public/app/app.js')
     .bundle()
     .pipe(source('main.js'))
-    .pipe(gulp.dest('./client/app/'));
+    .pipe(gulp.dest('./public/app/'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch('client/app/**/!(main).js', ['browserify']);
+  gulp.watch('public/app/**/!(main).js', ['browserify']);
 });
 
 gulp.task('default', ['connect', 'watch']);
