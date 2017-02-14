0b18d0ef3df5e564d56e4c31e61c0da44e94cf53
Jamie McDonald <jamie@qwyck.net>
2017-02-14 09:11:35 +0000

    Add POST to /api/elevations.

--- server/routes/elevations.js
+++ server/routes/elevations.js
@@ -12,10 +12,35 @@ router.get('/list', (req, res, next) => {
     res.status(400);
     res.send({ errors: [{ status: 400, title: 'Invalid query string', detail: 'Points parameter is expected and must not be empty.' }] });
   } else {
-    req.query.key = API_KEY;
-    req.pipe(request({ url: `${BASE_URL}/List`, qs: req.query, method: req.method })).pipe(res);
-  }
+    const options = {
+      url: `${BASE_URL}/List`,
+      qs: req.query,
+      method: req.method.toLowerCase(),
+    };
 
+    req.query.key = API_KEY;
+    req.pipe(request(options)).pipe(res);
+  }
+});
+
+router.post('/list', (req, res, next) => {
+  if (typeof req.body.points === 'undefined' || req.body.points === '') {
+    res.status(400);
+    res.send({ errors: [{ status: 400, title: 'Invalid body', detail: 'Points parameter is expected and must not be empty.' }] });
+  } else {
+    const pointString = `points=${req.body.points}`;
+    const options = {
+      url: `${BASE_URL}/List?key=${API_KEY}`,
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Content-Length': pointString.length,
+      },
+      body: pointString,
+      method: req.method.toLowerCase(),
+    };
+
+    req.pipe(request(options), { end: false }).pipe(res);
+  }
 });
 
 module.exports = router;
--- test/server/api/test-elevations.js
+++ test/server/api/test-elevations.js
@@ -48,41 +48,83 @@ describe('Elevations API Tests', function () {
     return expect(MUser.remove({})).to.be.fulfilled;
   });
 
-  it('should return status 400 error with no "points" parameter', function () {
-    const p = chai.request(app)
-      .get('/api/elevations/list')
-      .set('Authorization', `JWT ${token}`)
-      .catch((err) => {
-        expect(err).to.have.status(400);
-      });
+  describe('GET Tests', function () {
+    it('should return status 400 error with no "points" parameter', function () {
+      const p = chai.request(app)
+        .get('/api/elevations/list')
+        .set('Authorization', `JWT ${token}`)
+        .catch((err) => {
+          expect(err).to.have.status(400);
+        });
 
-    return expect(p).to.be.fulfilled;
+      return expect(p).to.be.fulfilled;
+    });
+
+    it('should return status 400 error with empty "points" parameter', function () {
+      const p = chai.request(app)
+        .get('/api/elevations/list')
+        .set('Authorization', `JWT ${token}`)
+        .query({ points: '' })
+        .catch((err) => {
+          expect(err).to.have.status(400);
+        });
+
+      return expect(p).to.be.fulfilled;
+    });
+
+    it('should return elevation data', function () {
+      const p = chai.request(app)
+        .get('/api/elevations/list')
+        .set('Authorization', `JWT ${token}`)
+        .query({ points: '53.06863553236679,-4.078014492988587' })
+        .then((res) => {
+          expect(res.body.resourceSets).to.have.length(1);
+          expect(res.body.resourceSets[0].resources).to.have.length(1);
+          expect(res.body.resourceSets[0].resources[0].elevations).to.have.length(1);
+          expect(res.body.resourceSets[0].resources[0].elevations[0]).to.equal(1023);
+        });
+
+      return expect(p).to.be.fulfilled;
+    });
   });
 
-  it('should return status 400 error with empty "points" parameter', function () {
-    const p = chai.request(app)
-      .get('/api/elevations/list')
-      .set('Authorization', `JWT ${token}`)
-      .query({ points: '' })
-      .catch((err) => {
-        expect(err).to.have.status(400);
-      });
+  describe('POST Tests', function () {
+    it('should return status 400 error with no "points" parameter', function () {
+      const p = chai.request(app)
+        .post('/api/elevations/list')
+        .set('Authorization', `JWT ${token}`)
+        .catch((err) => {
+          expect(err).to.have.status(400);
+        });
 
-    return expect(p).to.be.fulfilled;
-  });
+      return expect(p).to.be.fulfilled;
+    });
 
-  it('should return elevation data', function () {
-    const p = chai.request(app)
-      .get('/api/elevations/list')
-      .set('Authorization', `JWT ${token}`)
-      .query({ points: '53.06863553236679,-4.078014492988587' })
-      .then((res) => {
-        expect(res.body.resourceSets).to.have.length(1);
-        expect(res.body.resourceSets[0].resources).to.have.length(1);
-        expect(res.body.resourceSets[0].resources[0].elevations).to.have.length(1);
-        expect(res.body.resourceSets[0].resources[0].elevations[0]).to.equal(1023);
-      });
+    it('should return status 400 error with empty "points" parameter', function () {
+      const p = chai.request(app)
+        .post('/api/elevations/list')
+        .set('Authorization', `JWT ${token}`)
+        .send({ points: '' })
+        .catch((err) => {
+          expect(err).to.have.status(400);
+        });
 
-    return expect(p).to.be.fulfilled;
+      return expect(p).to.be.fulfilled;
+    });
+
+    it('should return elevation data', function () {
+      const p = chai.request(app)
+        .post('/api/elevations/list')
+        .set('Authorization', `JWT ${token}`)
+        .send({ points: '53.06863553236679,-4.078014492988587' })
+        .then((res) => {
+          expect(res.body.resourceSets).to.have.length(1);
+          expect(res.body.resourceSets[0].resources).to.have.length(1);
+          expect(res.body.resourceSets[0].resources[0].elevations).to.have.length(1);
+          expect(res.body.resourceSets[0].resources[0].elevations[0]).to.equal(1023);
+        });
+
+      return expect(p).to.be.fulfilled;
+    });
   });
 });
